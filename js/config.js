9e2c8b123c012a053aa6d9f1ae7aea531af01941
Hansa Themiya <hansathemiya50@gmail.com>
2026-03-28 21:40:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 
 const SUPABASE_URL = 'https://hsjhuiyyxotsyqjncvvm.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_LDPqrZ_lnAZMZIgOZ-6Qwg_VdQbMi6A'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhzamh1aXl5eG90c3lxam5jdnZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MTA0MDMsImV4cCI6MjA5MDI4NjQwM30.p-CXtR_bY20n1fq8hz1hvpWu9gNT3dWPoVs7NNiwhRY'; 
 
 // Create client with unique name to avoid conflict with CDN global
 const six7Supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
